25b5d27807277be6c6da9c9eca450602a8a3d278
Oscar Rieken <oscar.rieken@adp.com>
2019-02-03 15:49:38 -0500

    feat(RetweetCollection): added size and creation specs

---
+++ src/RetweetCollection/RetweetCollection.h
@@ -0,0 +1,18 @@
+//
+// Created by Rieken, Oscar (CORP) on 2019-02-03.
+//
+
+#ifndef RetweetCollection_h
+#define RetweetCollection_h
+
+class RetweetCollection {
+public:
+    bool isEmpty() const {
+        return 0 == size();
+    }
+
+    unsigned int size() const {
+        return 0;
+    }
+};
+#endif
