a39476b31fb913e55647db43692ac52c72529ce1
Oscar Rieken <oscar.rieken@adp.com>
2019-02-03 19:11:00 -0500

    test(RetweetCollectionTest): added Tweet dep and test for adding tweet

--- tests/RetweetCollectionTest.cpp
+++ tests/RetweetCollectionTest.cpp
@@ -1,5 +1,6 @@
 #include "gmock/gmock.h"
 #include "src/RetweetCollection/RetweetCollection.h"
+#include "src/Tweet/Tweet.h"
 
 using namespace ::testing;
 
@@ -7,10 +8,28 @@ class ARetweetCollection: public Test {
 public: RetweetCollection collection;
 };
 
+MATCHER_P(HasSize, expected, ""){
+    return
+    arg.size() == expected &&
+    arg.isEmpty() == (0 == expected);
+}
+
 TEST_F(ARetweetCollection, IsEmptyWhenCreated) {
     ASSERT_TRUE(collection.isEmpty());
 }
 
+TEST_F(ARetweetCollection, IsNoLongerEmptyAfterTweetAdded) {
+    collection.add(Tweet());
+
+    ASSERT_FALSE(collection.isEmpty());
+}
+
 TEST_F(ARetweetCollection, HasSizeZeroWhenCreated) {
-    ASSERT_THAT(collection.size(), Eq(0u));
+ASSERT_THAT(collection.size(), Eq(0u));
+}
+
+TEST_F(ARetweetCollection, HasSizeOfOneAfterTweetAdded) {
+    collection.add(Tweet());
+
+    ASSERT_THAT(collection.size(), Eq(1u));
 }
